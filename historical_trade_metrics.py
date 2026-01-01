4f7db5c78360d4206f18670315b67f6a76b8108e
abhixshek <abhishek.dce.2000@gmail.com>
2026-01-01 18:59:23 +0530

    code restructured-DRY

--- historical_trade_metrics.py
+++ historical_trade_metrics.py
@@ -1,3 +1,5 @@
+#!.venv/Scripts/python
+
 import os
 import sys
 import numpy as np
@@ -19,104 +21,85 @@ def calculate_ratios(no_of_losing_trades, no_of_winning_trades, avg_loss_pct, av
     return batting_avg, win_loss_ratio, adj_win_loss_ratio
 
 
-def calc_kite(pnl_file): # TODO maintain single calc function for all brokers with function signature as calculate_metrics(pnl_file, broker)
-    try:
-        metrics_df = pd.read_excel(Path('input') / pnl_file, skiprows=36, header=1, usecols='B:N')
-    except:
-        print(f'{pnl_file} not found.')
-        sys.exit()
+def read_pnl_file(pnl_file, broker):
+    if broker == 'kite':
+        try:
+            pnl_df = pd.read_excel(Path('input') / pnl_file, skiprows=36, header=1, usecols='B:N')
+        except:
+            print(f'{pnl_file} not found.')
+            sys.exit()
+    
+    elif broker == 'groww':
+        try:
+            pnl_df = pd.read_excel(Path('input') / pnl_file, skiprows=24)
+        except:
+            print(f'{pnl_file} not found.')
+            sys.exit()
 
-    loss_condition = metrics_df['Realized P&L'] <= 0
+        pnl_df = pnl_df.iloc[:pnl_df.index[pnl_df['Stock name'].isnull()][0], :]
+        pnl_df['Realized P&L Pct.'] = 100 * pnl_df.loc[:, 'Realised P&L'] / pnl_df.loc[:, 'Buy value']
+    
+    elif broker == 'dhan':
+        try:
+            pnl_df = pd.read_excel(Path('input') / pnl_file, skiprows=13)
+        except:
+            print(f'file {pnl_file} not found.')
+            sys.exit()
+        
+        pnl_df = pnl_df.iloc[: pnl_df.index[pnl_df['Sr.'] == 'F&O Segment'][0], :]
+        pnl_df = pnl_df.dropna(subset=['Sr.'])
+        pnl_df['Realized P&L Pct.'] = 100 * pnl_df.loc[:, 'Realised P&L'] / pnl_df.loc[:, 'Buy Value']
+    
+    pnl_df = pnl_df.rename(columns={"Realised P&L": "Realized P&L"})
+
+    return pnl_df
+
+
+def calculate_total_charges(pnl_file, broker):
+    if broker == 'kite':
+        all_charges = pd.read_excel(Path('input') / pnl_file, skiprows=13, nrows=2, usecols='B:C').iloc[:,1]
+        all_charges = all_charges.apply(lambda x: -x if x > 0 else x)
+        total_charges = all_charges.sum()
+
+    elif broker == 'groww':
+        groww_pnl = pd.read_excel(Path('input') / pnl_file, usecols="A:B", index_col=0, nrows=20)
+        total_charges = -groww_pnl.iloc[11:11+8,0].sum()
+    
+    elif broker == 'dhan':
+        dhan_pnl_df = pd.read_excel(Path('input') / pnl_file, skiprows=6, nrows=1)
+        total_charges = -dhan_pnl_df.loc[0, 'Total Charges']
+    
+    return total_charges
+
+
+def calculate_metrics(pnl_df, total_charges=0):
+    realized_pnl = total_charges + pnl_df['Realized P&L'].sum()
+
+    loss_condition = pnl_df['Realized P&L'] <= 0
     win_condition = np.logical_not(loss_condition)
     
     no_of_losing_trades = loss_condition.sum()
     no_of_winning_trades = win_condition.sum()
 
-    avg_loss = metrics_df.loc[loss_condition, 'Realized P&L'].mean()
-    avg_gain = metrics_df.loc[win_condition, 'Realized P&L'].mean()
+    avg_loss = pnl_df.loc[loss_condition, 'Realized P&L'].mean()
+    avg_gain = pnl_df.loc[win_condition, 'Realized P&L'].mean()
 
-    avg_loss_pct = metrics_df.loc[loss_condition, 'Realized P&L Pct.'].mean()
-    avg_gain_pct = metrics_df.loc[win_condition, 'Realized P&L Pct.'].mean()
+    avg_loss_pct = pnl_df.loc[loss_condition, 'Realized P&L Pct.'].mean()
+    avg_gain_pct = pnl_df.loc[win_condition, 'Realized P&L Pct.'].mean()
 
     batting_avg, win_loss_ratio, adj_win_loss_ratio = calculate_ratios(no_of_losing_trades, no_of_winning_trades,
                                                                        avg_loss_pct, avg_gain_pct)
-
-    realized_pnl = pd.read_excel(Path('input') / pnl_file, skiprows=13, nrows=3, usecols='B:C').iloc[:,1]
-    realized_pnl[0] = -1 * realized_pnl[0]
-    realized_pnl = realized_pnl.sum()
 
     return (no_of_losing_trades, no_of_winning_trades, avg_loss, avg_gain, avg_loss_pct, avg_gain_pct,
             batting_avg, win_loss_ratio, adj_win_loss_ratio, realized_pnl)
 
 
-def calc_groww(groww_pnl_file):
-    try:
-        groww_metrics_df = pd.read_excel(Path('input') / groww_pnl_file, skiprows=24)
-    except:
-        print(f'{groww_pnl_file} not found.')
-        sys.exit()
+def create_historical_df(pnl_file, date, broker):
+    pnl_df = read_pnl_file(pnl_file, broker)
+    total_charges = calculate_total_charges(pnl_file, broker)
 
-    groww_metrics_df = groww_metrics_df.iloc[:groww_metrics_df.index[groww_metrics_df['Stock name'].isnull()][0], :]
-    groww_metrics_df['Realised P&L%'] = 100 * groww_metrics_df.loc[:, 'Realised P&L'] / groww_metrics_df.loc[:, 'Buy value']
-
-    loss_condition = groww_metrics_df['Realised P&L'] <= 0
-    win_condition = np.logical_not(loss_condition)
-
-    no_of_losing_trades = loss_condition.sum()
-    no_of_winning_trades = win_condition.sum()
-    
-    avg_loss = groww_metrics_df.loc[loss_condition, 'Realised P&L'].mean()
-    avg_gain = groww_metrics_df.loc[win_condition, 'Realised P&L'].mean()
-    
-    avg_loss_pct = groww_metrics_df.loc[loss_condition, 'Realised P&L%'].mean()
-    avg_gain_pct = groww_metrics_df.loc[win_condition, 'Realised P&L%'].mean()
-
-    batting_avg, win_loss_ratio, adj_win_loss_ratio = calculate_ratios(no_of_losing_trades, no_of_winning_trades,
-                                                                       avg_loss_pct, avg_gain_pct)
-    
-    groww_pnl = pd.read_excel(Path('input') / groww_pnl_file, usecols="A:B", index_col=0, nrows=20)
-    realized_pnl = -groww_pnl.iloc[11:11+8,0].sum() + groww_metrics_df['Realised P&L'].sum()
-    
-    return (no_of_losing_trades, no_of_winning_trades, avg_loss, avg_gain, avg_loss_pct, avg_gain_pct,
-            batting_avg, win_loss_ratio, adj_win_loss_ratio, realized_pnl)
-
-
-def calc_dhan(dhan_pnl_file='PNL_REPORT.xls'):
-    try:
-        dhan_df = pd.read_excel(Path('input') / 'PNL_REPORT.xls', skiprows=13)
-    except:
-        print(f'file {dhan_pnl_file} not found.')
-        sys.exit()
-
-    dhan_df = dhan_df.iloc[: dhan_df.index[dhan_df['Sr.'] == 'F&O Segment'][0], :]
-    dhan_df = dhan_df.dropna(subset=['Sr.'])
-    dhan_df['Realised P&L%'] = 100 * dhan_df.loc[:, 'Realised P&L'] / dhan_df.loc[:, 'Buy Value']
-
-    loss_condition = dhan_df['Realised P&L'] <= 0
-    win_condition = np.logical_not(loss_condition)
-
-    no_of_losing_trades = loss_condition.sum()
-    no_of_winning_trades = win_condition.sum()
-
-    avg_loss = dhan_df.loc[loss_condition, 'Realised P&L'].mean()
-    avg_gain = dhan_df.loc[win_condition, 'Realised P&L'].mean()
-
-    avg_loss_pct = dhan_df.loc[loss_condition, 'Realised P&L%'].mean()
-    avg_gain_pct = dhan_df.loc[win_condition, 'Realised P&L%'].mean()
-
-    batting_avg, win_loss_ratio, adj_win_loss_ratio = calculate_ratios(no_of_losing_trades, no_of_winning_trades,
-                                                                       avg_loss_pct, avg_gain_pct)
-
-    dhan_pnl_df = pd.read_excel(Path('input') / 'PNL_REPORT.xls', skiprows=6, nrows=1)
-    realized_pnl = -dhan_pnl_df.loc[0, 'Total Charges'] + sum(dhan_df['Realised P&L'])
-
-    return (no_of_losing_trades, no_of_winning_trades, avg_loss, avg_gain, avg_loss_pct, avg_gain_pct,
-            batting_avg, win_loss_ratio, adj_win_loss_ratio, realized_pnl)
-
-
-def create_historical_df(pnl_file, date, broker, calc_function):
     (no_of_losing_trades, no_of_winning_trades, avg_loss, avg_gain, avg_loss_pct,
-     avg_gain_pct, batting_avg, win_loss_ratio, adj_win_loss_ratio, realized_pnl) = calc_function(pnl_file)
+     avg_gain_pct, batting_avg, win_loss_ratio, adj_win_loss_ratio, realized_pnl) = calculate_metrics(pnl_df, total_charges)
     
     y = [(date, no_of_losing_trades, no_of_winning_trades, avg_loss, avg_gain, avg_loss_pct, avg_gain_pct,
           batting_avg, win_loss_ratio, adj_win_loss_ratio, realized_pnl)]
@@ -169,17 +152,10 @@ if __name__ == "__main__":
         'dhan': dhan_pnl_file
     }
 
-    calc_functions = {
-        'kite': calc_kite,
-        'groww': calc_groww,
-        'dhan': calc_dhan
-    }
-
     for broker in broker_args_passed:
         print(f"Running through {broker[0]} data")
         pnl_file = input_pnl_files[broker[0]]
-        calc_function = calc_functions[broker[0]]
-        rba_df = create_historical_df(pnl_file, date, broker[0], calc_function)
+        rba_df = create_historical_df(pnl_file, date, broker[0])
         rba_df.to_csv(Path('output') / f'trade_metrics_{broker[0]}.csv', index=False)
         try:
             os.remove(os.path.join(os.getcwd(), 'input', pnl_file))
